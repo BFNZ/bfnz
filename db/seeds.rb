090f9acb13f25c205a87772df76ff4fe14938013
Tim Gunther <tim.bfnz@gmail.com>
2015-10-13 21:12:35 +1300

    re seeded for new descriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -83,7 +83,7 @@ end
  ['R', "The New Testament Recovery Version", nil, "the_new_testament.jpg", "The New Testament Recovery Version is a comprehensive study Bible accurately translated from the original Greek text into modern English. It features extensive notes emphasizing the revelation of the truth, outlines of each book, cross-references, charts and maps, and more."],
  ['A', "The Christian Experience Set", "Witness Lee", "christian_experience.jpg", "Lee shows that the good land in all its aspects is actually a type of Christ in His all-inclusiveness. In this book, the riches of the good land are explained and applied to our experience of Christ today."],
  ['G', "The Church Set", "Watchman Nee & Witness Lee", "church_set.jpg", "What is the the church and what is its purpose? Watchman Nee uses types of the church from throughout the Bible to show us the church's origin, purpose, and destiny."],
- ['B', "Basic Elements of the Christian Life", "Watchman Nee and Witness Lee", "basic_elements.jpg", "This series details the basic yet crucial elements of the life of a Christian, showing how each provides the foundation for a rich and meaningful Christian life."],
+ ['B', "Foundational Christian Truths Set", "Watchman Nee and Witness Lee", "basic_elements.jpg", "This series details the basic yet crucial elements of the life of a Christian, showing how each provides the foundation for a rich and meaningful Christian life."],
 ].each do |code, title, author, image_path, description|
    Item.create!(code: code, title: title, author: author, image_path: image_path, description: description) unless Item.find_by_code(code)
  end
