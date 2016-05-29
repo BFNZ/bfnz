e2fb5cc69bc248c6174b67419a89264015545149
Shevaun Coker <shevaun.coker@envato.com>
2016-05-29 16:41:15 +1000

    Upgrade Rails - which somehow fixed the JS running in the capybara specs!

--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -1,12 +1,12 @@
 module FeatureHelpers
   def select_address(address)
     fill_in "Address", with: address
-  #  find("li.af_item:first-child").click
+    find("li.af_item:first-child").click
   end
 
   def select_item
-    select "The New Testament Recovery Version", from: "item_list"
-   # page.first(".image_picker_image").click
+    #select "The New Testament Recovery Version", from: "item_list"
+    page.first(".image_picker_image").click
   end
 
   def login_as_admin
