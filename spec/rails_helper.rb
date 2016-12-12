734c3cd832826baf05d52d58c68b5c4799567dcb
Paul Salmon <paulesalmon@gmail.com>
2016-12-12 23:06:59 +1300

    added config for rspec to run with firefox47

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,13 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
-require 'spec_helper'
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'database_cleaner'
 require 'webmock/rspec'
 require 'capybara/rails'
+require 'support/pages/capybara_page'
+require 'selenium-webdriver'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -22,8 +24,20 @@ ActiveRecord::Migration.maintain_test_schema!
 
 Capybara.javascript_driver = :selenium
 
+# Current Firefox version not working with webdriver
+# Configuration to specify an older version of firefox
+# http://blog.pixarea.com/2013/02/locking-the-firefox-version-when-testing-rails-with-capybara-and-selenium/
+# Older version of firefox can be found https://ftp.mozilla.org/pub/firefox/releases/47.0.1/
+
+Capybara.register_driver :selenium do |app|
+  require 'selenium/webdriver'
+  Selenium::WebDriver::Firefox::Binary.path = "/opt/firefox47/bin/firefox"
+  Capybara::Selenium::Driver.new(app, :browser => :firefox)
+end
+
 RSpec.configure do |config|
   config.include FeatureHelpers, type: :feature
+  #webdriver.firefox.bin
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
@@ -53,6 +67,9 @@ RSpec.configure do |config|
     # stub_request(:any, 'api.addressfinder.nz').
     #   to_return(%Q{/**/reqwest_1427708603773({"completions":[{"a":"5 Oxford Avenue, Kamo, Whangarei 0112","pxid":"2-.9.2U.4.1Z.5","v":1},{"a":"5 Oxford Court, Paraparaumu 5032","pxid":"2-.F.w.5.1Z.3","v":1},{"a":"5 Oxford Crescent, Ebdentown, Upper Hutt 5018","pxid":"2-.F.1P.8.M.q","v":1},{"a":"5 Oxford Road, Manurewa, Auckland 2102","pxid":"2-.1.6.1E.2X.C","v":1},{"a":"5 Oxford Road, Rangiora 7400","pxid":"2-.3.3L.2.4k.D","v":1},{"a":"5 Oxford Road, Springvale, Wanganui 4501","pxid":"2-.6.2F.C.V.4","v":1},{"a":"5 Oxford Street, Balclutha 9230","pxid":"2-.A.E.1.2n.8","v":1},{"a":"5 Oxford Street, Fairfield, Hamilton 3214","pxid":"2-.E.Y.C.d.V","v":1},{"a":"5 Oxford Street, Hampstead, Ashburton 7700","pxid":"2-.3.D.4.Q.19","v":1},{"a":"5 Oxford Street, Hokowhitu, Palmerston North 4410","pxid":"2-.6.1I.7.10.a","v":1}], "paid":false})})
   end
+
+
+
 
   config.around(:each) do |example|
     DatabaseCleaner.cleaning do
