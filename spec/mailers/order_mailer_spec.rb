94e4ded0576fc89680467c48287926b84413bc5b
Paul Salmon <paulesalmon@gmail.com>
2016-12-18 14:57:07 +1300

    Updated tests with correct email.  Changed book set name.

--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe OrderMailer do
     let(:customer) { Customer.new(email: 'myemail@gmail.com', first_name: 'Jane', last_name: 'Doe') }
     let(:order) { Order.new(customer: customer) }
 
-    it { expect(email.from).to eq ["info@biblesfornewzealand.org.nz"] }
+    it { expect(email.from).to eq ["info@bfnz.org.nz"] }
     it { expect(email.to).to eq ['myemail@gmail.com'] }
     it { expect(email.subject).to eq "Thank you for your request for free literature from Bibles for New Zealand" }
     it { expect(email.parts[0].body.to_s).to match /Dear Jane Doe/ }
