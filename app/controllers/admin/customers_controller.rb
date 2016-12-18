88f7abfc3e372ca7bc99fbec1c5fdaf765c95944
Ducksan Cho <ducktyper@gmail.com>
2016-12-18 21:47:55 +1300

    Clean code

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -12,13 +12,8 @@ module Admin
       if create_customer.success?
         redirect_to new_admin_customer_path, notice: "Customer created successfully. Customer Id: #{view_context.link_to(create_customer.cust_id, edit_admin_customer_path(create_customer.cust_id))}"
       else
-        @duplicates_by_name = duplicates_by_name(
-          @new_customer_form.first_name,
-          @new_customer_form.last_name
-        )
-        @duplicates_by_address = duplicates_by_address(
-          @new_customer_form.pxid
-        )
+        @duplicates_by_name    = duplicates_by_name(@new_customer_form.first_name, @new_customer_form.last_name)
+        @duplicates_by_address = duplicates_by_address(@new_customer_form.pxid)
         render :new
       end
     end
@@ -39,12 +34,8 @@ module Admin
     end
 
     def find_duplicate_by_name_or_address
-      @duplicates_by_name = duplicates_by_name(
-        params[:first_name], params[:last_name]
-      )
-      @duplicates_by_address = duplicates_by_address(
-        params[:pxid]
-      )
+      @duplicates_by_name    = duplicates_by_name(params[:first_name], params[:last_name])
+      @duplicates_by_address = duplicates_by_address(params[:pxid])
     end
 
     def merge
