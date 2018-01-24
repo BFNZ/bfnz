f0e6f8e63588295c58f2facb223d1d0c14bfcff6
Ducksan Cho <ducktyper@gmail.com>
2018-01-25 01:07:17 +1300

    Fix all tests

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,7 +4,7 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-    @user_session = UserSession.new(params[:user_session])
+    @user_session = UserSession.new(user_session_params)
     if @user_session.save
       redirect_back_or_default admin_root_path
     else
@@ -18,5 +18,11 @@ class UserSessionsController < ApplicationController
     @user_session.destroy
     flash[:notice] = "Successfully logged out."
     redirect_to root_path
+  end
+
+  private
+
+  def user_session_params
+    params.require(:user_session).permit(:email, :password)
   end
 end
