d423e9b2e8c1b0ad9c416335090658ea5dc125b3
Ducksan Cho <ducktyper@gmail.com>
2018-07-22 16:29:22 +1200

    Pass tests
    Use in_time_zone to parse date to NZ time zone rather than SYSTEM time zone (UTC)

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,8 +11,8 @@ class Order < ActiveRecord::Base
   enum method_of_discovery: [:unknown, :mail_disc, :uni_lit, :non_uni_lit, :other_ad, :word_of_mouth, :website, :other_disc, :table_disc]
   enum method_received: [:mail, :phone, :personally_delivered, :internet, :other, :table]
 
-  scope :created_between, ->(from, to) { where("orders.created_at BETWEEN ? AND ?", from.to_time, to.to_time+1.day) }
-  scope :shipped_between, ->(from, to) { joins(:shipment).where("shipments.created_at BETWEEN ? AND ?", from.to_time, to.to_time+1.day) }
+  scope :created_between, ->(from, to) { where("orders.created_at >= ? AND orders.created_at < ?", from.in_time_zone, to.in_time_zone + 1.day) }
+  scope :shipped_between, ->(from, to) { joins(:shipment).where("shipments.created_at >= ? AND shipments.created_at < ?", from.in_time_zone, to.in_time_zone + 1.day) }
   scope :id, ->(id) { where(id: id) }
   scope :shipped, -> { where("orders.shipment_id IS NOT NULL") }
   scope :ready_to_ship, -> {
