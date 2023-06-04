eb89c04cd69453d34a022305304e13210716622e
Paul Salmon <paulesalmon@gmail.com>
2023-06-04 22:00:55 +1200

    Fix issue with cancel button /timezone

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Bfnz
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -20,5 +21,18 @@ module Bfnz
     config.time_zone = 'Wellington'
     
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # List of classes deemed safe to load by YAML, and required by the Audited
+# gem when deserialized audit records.
+# As of Rails 6.0.5.1, YAML safe-loading method does not allow all classes
+# to be deserialized by default: https://discuss.rubyonrails.org/t/cve-2022-32224-possible-rce-escalation-bug-with-serialized-columns-in-active-record/81017
+  config.active_record.yaml_column_permitted_classes = [
+    ActiveSupport::HashWithIndifferentAccess,
+    ActiveSupport::TimeWithZone,
+    ActiveSupport::TimeZone,
+    Date,
+    Time,
+    Symbol
+  ]
   end
 end
