fcc232aabec3af89a7be5896a89a5dda95508f93
Bharathi Beyond <bharathi@beyondfinity.com>
2023-10-26 21:14:45 +0530

    feat: remove virtus gem from codebase

--- app/forms/admin/existing_customer_form.rb
+++ app/forms/admin/existing_customer_form.rb
@@ -1,34 +1,17 @@
 module Admin
   class ExistingCustomerForm < BaseForm
-    attribute :title, String
-    attribute :first_name, String
-    attribute :last_name, String
 
-    attribute :address, String
-    attribute :suburb, String
-    attribute :city_town, String
-    attribute :post_code, String
-    attribute :pxid, String
-    attribute :dpid, String
-    attribute :x, Decimal
-    attribute :y, Decimal
-    attribute :ta, String
-    attribute :bad_address, Virtus::Attribute::Boolean
+    attr_accessor :title, :first_name, :last_name, :address, :suburb, :city_town, 
+                  :post_code, :pxid, :dpid, :x, :y, :ta, :bad_address, :phone, :email, 
+                  :tertiary_student, :tertiary_institution, :admin_notes, 
+                  :further_contact_requested
 
-    attribute :phone, String
-    attribute :email, String
-
-    attribute :tertiary_student, Virtus::Attribute::Boolean
-    attribute :tertiary_institution, String
-
-    attribute :admin_notes, String
-    attribute :further_contact_requested, Integer
+    attr_reader :customer, :confirm_personal_order
 
     validates :first_name, :last_name, :ta, presence: true
     validates :confirm_personal_order, acceptance: true
 
 
-    attr_reader :customer
 
     CUSTOMER_ATTRS = %w{title first_name last_name address suburb city_town post_code pxid dpid x y  ta bad_address phone email tertiary_student tertiary_institution further_contact_requested admin_notes}
 
@@ -44,7 +27,7 @@ module Admin
     private
 
     def customer_attributes
-      customer.attributes_for_database.slice(*CUSTOMER_ATTRS)
+      customer.attributes.symbolize_keys.slice(*CUSTOMER_ATTRS.map(&:to_sym))
     end
   end
 end
