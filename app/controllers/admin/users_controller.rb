3cad45771243faa59aeb98c429d53a3db41fa8a1
jsmld <samvanerpersroijaards@gmail.com>
2024-06-14 17:56:30 -0300

    superadmins can't change each others passwords, restructure Users view

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,17 +1,17 @@
 class Admin::UsersController < Admin::BaseController
   before_action :require_user
   before_action :require_superadmin, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update]
+  before_action :prevent_superadmin_edit, only: [:edit, :update]
 
   def index
     @users = User.all
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to admin_users_path, notice: 'Password updated successfully.'
     else
@@ -39,6 +39,17 @@ class Admin::UsersController < Admin::BaseController
   def require_superadmin
     unless current_user.superadmin?
       flash[:error] = "You must be a superadmin to access this page"
+      redirect_to admin_users_path
+    end
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def prevent_superadmin_edit
+    if @user.superadmin? && @user != current_user
+      flash[:error] = "You cannot edit another superadmin's password"
       redirect_to admin_users_path
     end
   end
